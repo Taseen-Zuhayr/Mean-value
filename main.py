17a0c892b7d13afb895949004e725f2db98c7802
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-09-21 23:04:16 +0600

    mean value

---
+++ main.py
@@ -0,0 +1,13 @@
+meanwrong = 38
+wrongnum = 36
+rightnum = 56
+total = 40
+
+sum = meanwrong*total
+print("The added value is",sum)
+
+num2 = sum - ((wrongnum)-(rightnum))
+print("The new number is",num2)
+
+rightmean = num2/total
+print("The correct mean is",rightmean)
